1879ccc98b3ec086461fef23777028d581710dfc
power_840 <liuyu19921017@gmail.com>
2021-12-02 15:52:34 +0800

    feat: update 20211202

---
+++ 20211129/1.CapitalizeString.ts
@@ -0,0 +1,17 @@
+/**
+ * 
+    首字母大写
+
+    ```TypeScript
+    type a1 = CapitalizeString<'handler'>       // Handler
+    type a2 = CapitalizeString<'parent'>        // Parent
+    type a3 = CapitalizeString<233>             // 233
+    ```
+
+ */
+
+// type a1 = CapitalizeString<'handler'>       // Handler
+// type a2 = CapitalizeString<'parent'>        // Parent
+// type a3 = CapitalizeString<233>             // 233
+
+// type CapitalizeString<T extends string> =
